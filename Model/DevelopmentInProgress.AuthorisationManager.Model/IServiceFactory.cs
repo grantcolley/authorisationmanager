e96c71a23a0a2054be8dd161acd5578e34d6bb7c
grantcolley <grantcolley@yahoo.com>
2016-03-21 14:49:01 +0000

    Add the service factory interface to the model
    Add the service factory interface to the model

---
+++ Model/DevelopmentInProgress.AuthorisationManager.Model/IServiceFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DevelopmentInProgress.DipSecure;
+
+namespace DevelopmentInProgress.AuthorisationManager.Model
+{
+    public interface IServiceFactory
+    {
+        IList<Activity> GetActivities();
+        IList<Role> GetRoles();
+        IList<UserAuthorisation> GetUserAuthorisations();
+    }
+}
