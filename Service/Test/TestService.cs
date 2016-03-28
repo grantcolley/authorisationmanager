1fa6dbc1a1f76998fd2bc49ee296687c7e89163f
grantcolley <grantcolley@yahoo.com>
2016-03-28 22:21:55 +0100

    Refactor service to use IAuthorisationManagerService
    Refactor service to use IAuthorisationManagerService

--- Service/Test/TestService.cs
+++ Service/Test/TestService.cs
@@ -1,44 +1,48 @@
-﻿using System.Collections.Generic;
-using DevelopmentInProgress.AuthorisationManager.Model;
-using DevelopmentInProgress.DipSecure;
+﻿using System;
 
 namespace DevelopmentInProgress.AuthorisationManager.Service.Test
 {
-    public class TestService: IServiceFactory
+    public class TestService : IAuthorisationManagerService
     {
-        public IList<Activity> GetActivities()
+        public string GetActivities()
         {
-            var activity1 = new Activity() { Id = 0, Name = "Read", ActivityCode = "READ" };
-            var activity2 = new Activity() { Id = 1, Name = "Write", ActivityCode = "WRITE" };
+            //var activity1 = new Activity() { Id = 0, Name = "Read", ActivityCode = "READ" };
+            //var activity2 = new Activity() { Id = 1, Name = "Write", ActivityCode = "WRITE" };
 
-            return new List<Activity>(new[] { activity1, activity2 });
+            //return new List<Activity>(new[] { activity1, activity2 });
+
+            return String.Empty;
         }
 
-        public IList<Role> GetRoles()
+        public string GetRoles()
         {
-            var activities = GetActivities();
+            //var activities = GetActivities();
 
-            var role1 = new Role() {Id = 0, Name = "Reader", RoleCode = "READER"};
-            role1.Activities.Add(activities[0]);
+            //var role1 = new Role() {Id = 0, Name = "Reader", RoleCode = "READER"};
+            //role1.Activities.Add(activities[0]);
 
-            var role2 = new Role() { Id = 1, Name = "Writer", RoleCode = "WRITER" };
-            role1.Activities.Add(activities[0]);
-            role2.Activities.Add(activities[1]);
+            //var role2 = new Role() { Id = 1, Name = "Writer", RoleCode = "WRITER" };
+            //role1.Activities.Add(activities[0]);
+            //role2.Activities.Add(activities[1]);
 
-            return new List<Role>(new[] {role1, role2});
+            //return new List<Role>(new[] {role1, role2});
+
+            return String.Empty;
         }
 
-        public IList<UserAuthorisation> GetUserAuthorisations()
+        public string GetUserAuthorisations()
         {
-            var roles = GetRoles();
+            //var roles = GetRoles();
 
-            var user1 = new UserAuthorisation("gcolley") {DisplayName = "Grant Colley"};
-            user1.Roles.Add(roles[0]);
+            //var user1 = new UserAuthorisation("gcolley") {DisplayName = "Grant Colley"};
+            //user1.Roles.Add(roles[0]);
 
-            var user2 = new UserAuthorisation("mcolley") { DisplayName = "Melanie Colley" };
-            user2.Roles.Add(roles[1]);
+            //var user2 = new UserAuthorisation("mcolley") { DisplayName = "Melanie Colley" };
+            //user2.Roles.Add(roles[1]);
 
-            return new List<UserAuthorisation>(new[] {user1, user2});
+            //return new List<UserAuthorisation>(new[] {user1, user2});
+
+            return String.Empty;
         }
     }
 }
