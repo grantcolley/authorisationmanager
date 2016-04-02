9701bf15fc66c36727eb1e60c265f0bcfa8c9e75
grantcolley <grantcolley@yahoo.com>
2016-04-02 20:36:46 +0100

    Remove reference to obsolete Microsoft.Practices.ObjectBuilder2
    Remove reference to obsolete Microsoft.Practices.ObjectBuilder2

--- UI/WPF/Model/AuthorisationManagerService.cs
+++ UI/WPF/Model/AuthorisationManagerService.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Linq;
 using DevelopmentInProgress.AuthorisationManager.Service;
 using DevelopmentInProgress.DipSecure;
-using Microsoft.Practices.ObjectBuilder2;
 
 namespace DevelopmentInProgress.AuthorisationManager.WPF.Model
 {
@@ -19,7 +18,7 @@ namespace DevelopmentInProgress.AuthorisationManager.WPF.Model
         {
             var activityNodes = new List<ActivityNode>();
             var activities = authorisationManagerServiceProxy.GetActivities();
-            activities.ForEach(a => activityNodes.Add(GetActivityNode(a)));
+            activities.ToList().ForEach(a => activityNodes.Add(GetActivityNode(a)));
             return activityNodes;
         }
 
@@ -27,7 +26,7 @@ namespace DevelopmentInProgress.AuthorisationManager.WPF.Model
         {
             var roleNodes = new List<RoleNode>();
             var roles = authorisationManagerServiceProxy.GetRoles();
-            roles.ForEach(r => roleNodes.Add(GetRoleNode(r)));
+            roles.ToList().ForEach(r => roleNodes.Add(GetRoleNode(r)));
             return roleNodes;
         }
 
@@ -35,29 +34,29 @@ namespace DevelopmentInProgress.AuthorisationManager.WPF.Model
         {
             var userNodes = new List<UserNode>();
             var userAuthorisations = authorisationManagerServiceProxy.GetUserAuthorisations();
-            userAuthorisations.ForEach(u => userNodes.Add(GetUserNode(u)));
+            userAuthorisations.ToList().ForEach(u => userNodes.Add(GetUserNode(u)));
             return userNodes;
         }
 
         private ActivityNode GetActivityNode(Activity activity)
         {
-            var activityNode = new ActivityNode() { Id = activity.Id, Text = activity.Name };
-            activity.Activities.ForEach(a => activityNode.Activities.Add(GetActivityNode(a)));
+            var activityNode = new ActivityNode(activity);
+            activity.Activities.ToList().ForEach(a => activityNode.Activities.Add(GetActivityNode(a)));
             return activityNode;
         }
 
         private RoleNode GetRoleNode(Role role)
         {
-            var roleNode = new RoleNode() {Id = role.Id, Text = role.Name};
-            role.Activities.ForEach(a => roleNode.Activities.Add(GetActivityNode(a)));
-            role.Roles.ForEach(r => roleNode.Roles.Add(GetRoleNode(r)));
+            var roleNode = new RoleNode(role);
+            role.Activities.ToList().ForEach(a => roleNode.Activities.Add(GetActivityNode(a)));
+            role.Roles.ToList().ForEach(r => roleNode.Roles.Add(GetRoleNode(r)));
             return roleNode;
         }
 
         private UserNode GetUserNode(UserAuthorisation userAuthorisation)
         {
-            var userNode = new UserNode() {Id = userAuthorisation.Id, Text = userAuthorisation.DisplayName};
-            userAuthorisation.Roles.ForEach(r => userNode.Roles.Add(GetRoleNode(r)));
+            var userNode = new UserNode(userAuthorisation);
+            userAuthorisation.Roles.ToList().ForEach(r => userNode.Roles.Add(GetRoleNode(r)));
             return userNode;
         }
     }
