9010d6b63ed096748e0ad223dc1042924c1ca185
grantcolley <grantcolley@yahoo.com>
2016-06-21 22:26:30 +0100

    Create SQLSever project
    Create SQLSever project

---
+++ Data/DevelopmentInProgress.AuthorisationManager.Data.SQLServer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentInProgress.AuthorisationManager.Data.SQLServer
+{
+    public class Class1
+    {
+    }
+}
