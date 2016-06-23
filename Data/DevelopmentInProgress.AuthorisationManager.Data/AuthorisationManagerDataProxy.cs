d55e5433436b3eafd03434822c95440b990c6a50
grantcolley <grantcolley@yahoo.com>
2016-06-23 19:34:38 +0100

    Work in progress
    Work in progress

---
+++ Data/DevelopmentInProgress.AuthorisationManager.Data/AuthorisationManagerDataProxy.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using DevelopmentInProgress.DipSecure;
+
+namespace DevelopmentInProgress.AuthorisationManager.Data
+{
+    public class AuthorisationManagerDataProxy : IAuthorisationManagerServiceDataProxy
+    {
+        private readonly IAuthorisationManagerServiceData authorisationManagerServiceData;
+
+        public AuthorisationManagerDataProxy(IAuthorisationManagerServiceData authorisationManagerServiceData)
+        {
+            this.authorisationManagerServiceData = authorisationManagerServiceData;
+        }
+
+        public IList<Activity> GetActivities()
+        {
+            return authorisationManagerServiceData.GetActivities();
+        }
+
+        public IList<Role> GetRoles()
+        {
+            return authorisationManagerServiceData.GetRoles();
+        }
+
+        public IList<UserAuthorisation> GetUserAuthorisations()
+        {
+            return authorisationManagerServiceData.GetUserAuthorisations();
+        }
+
+        public Activity SaveActivity(Activity activity)
+        {
+            return authorisationManagerServiceData.SaveActivity(activity);
+        }
+
+        public Role SaveRole(Role role)
+        {
+            return authorisationManagerServiceData.SaveRole(role);
+        }
+
+        public UserAuthorisation SaveUserAuthorisation(UserAuthorisation userAuthorisation)
+        {
+            return authorisationManagerServiceData.SaveUserAuthorisation(userAuthorisation);
+        }
+
+        public bool DeleteActivity(int id)
+        {
+            return authorisationManagerServiceData.DeleteActivity(id);
+        }
+
+        public bool DeleteRole(int id)
+        {
+            return authorisationManagerServiceData.DeleteRole(id);
+        }
+
+        public bool DeleteUserAuthorisation(int id)
+        {
+            return authorisationManagerServiceData.DeleteUserAuthorisation(id);
+        }
+
+        public bool RemoveActivityFromActivity(int activityId, int parentId)
+        {
+            return authorisationManagerServiceData.RemoveActivityFromActivity(activityId, parentId);
+        }
+
+        public bool RemoveActivityFromRole(int activityId, int roleId)
+        {
+            return authorisationManagerServiceData.RemoveActivityFromRole(activityId, roleId);
+        }
+
+        public bool RemoveRoleFromRole(int roleId, int parentId)
+        {
+            return authorisationManagerServiceData.RemoveRoleFromRole(roleId, parentId);
+        }
+
+        public bool RemoveRoleFromUser(int roleId, int userId)
+        {
+            return authorisationManagerServiceData.RemoveRoleFromUser(roleId, userId);
+        }
+
+        public bool AddActivityToRole(int roleId, int activityId)
+        {
+            return authorisationManagerServiceData.AddActivityToRole(roleId, activityId);
+        }
+
+        public bool AddActivityToActivity(int parentActivityId, int activityId)
+        {
+            return authorisationManagerServiceData.AddActivityToActivity(parentActivityId, activityId);
+        }
+
+        public bool AddRoleToUser(int userId, int roleId)
+        {
+            return authorisationManagerServiceData.AddRoleToUser(userId, roleId);
+        }
+
+        public bool AddRoleToRole(int parentRoleId, int roleId)
+        {
+            return authorisationManagerServiceData.AddRoleToRole(parentRoleId, roleId);
+        }
+    }
+}
---
+++ Data/DevelopmentInProgress.AuthorisationManager.Data/IAuthorisationManagerServiceDataProxy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using DevelopmentInProgress.DipSecure;
+
+namespace DevelopmentInProgress.AuthorisationManager.Data
+{
+    public interface IAuthorisationManagerServiceDataProxy
+    {
+        IList<Activity> GetActivities();
+        IList<Role> GetRoles();
+        IList<UserAuthorisation> GetUserAuthorisations();
+        Activity SaveActivity(Activity activity);
+        Role SaveRole(Role role);
+        UserAuthorisation SaveUserAuthorisation(UserAuthorisation userAuthorisation);
+        bool DeleteActivity(int id);
+        bool DeleteRole(int id);
+        bool DeleteUserAuthorisation(int id);
+        bool RemoveActivityFromActivity(int activityId, int parentId);
+        bool RemoveActivityFromRole(int activityId, int roleId);
+        bool RemoveRoleFromRole(int roleId, int parentId);
+        bool RemoveRoleFromUser(int roleId, int userId);
+        bool AddActivityToRole(int roleId, int activityId);
+        bool AddActivityToActivity(int parentActivityId, int activityId);
+        bool AddRoleToUser(int userId, int roleId);
+        bool AddRoleToRole(int parentRoleId, int roleId);
+    }
+}
