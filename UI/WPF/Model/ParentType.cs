501544b691442857174c7c59877c99d7328ca1c4
grantcolley <grantcolley@yahoo.com>
2016-11-02 21:03:48 +0000

    Prpare to refactor parent to resolve serialising issues.
    Prpare to refactor parent to resolve serialising issues.

---
+++ UI/WPF/Model/ParentType.cs
@@ -0,0 +1,10 @@
+﻿namespace DevelopmentInProgress.AuthorisationManager.WPF.Model
+{
+    public enum ParentType
+    {
+        None = 0,
+        Activity = 1,
+        Role = 2,
+        User = 3
+    }
+}
