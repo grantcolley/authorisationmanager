c44e95c6da47f5173c3cfb18b36812fa8d59ed9d
grantcolley <grantcolley@yahoo.com>
2016-11-22 13:28:11 +0000

    Create the self hosting service.
    Create the self hosting service.

--- Service/DevelopmentInProgress.AuthorisationManager.WCF/Program.cs
+++ Service/DevelopmentInProgress.AuthorisationManager.WCF/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using DevelopmentInProgress.AuthorisationManager.Server;
+using DevelopmentInProgress.AuthorisationManager.Service;
 
 namespace DevelopmentInProgress.AuthorisationManager.WCF
 {
@@ -10,6 +10,40 @@ namespace DevelopmentInProgress.AuthorisationManager.WCF
     {
         static void Main(string[] args)
         {
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/AuthorisationManager");
+            Console.WriteLine("Created URI to server as base address: http://localhost:8733/Design_Time_Addresses/AuthorisationManager");
+        
+            ServiceHost selfHost = new ServiceHost(typeof (AuthorisationManagerServer), baseAddress);
+            Console.WriteLine("Create the service host: AuthorisationManagerServer.");
+
+            try
+            {
+                var unityBootstrapper = new UnityBootstrapper();
+                unityBootstrapper.Run();
+
+                selfHost.AddServiceEndpoint(typeof(IAuthorisationManagerService), new WSHttpBinding(), "AuthorisationManagerService");
+                Console.WriteLine("Add the service endpoint: AuthorisationManagerService.");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior() {HttpGetEnabled = true};
+                selfHost.Description.Behaviors.Add(smb);
+                Console.WriteLine("Service metadata exchange behavior enabled.");
+
+                selfHost.Description.Behaviors.Add(new UnityServiceBehavior(unityBootstrapper.Container));
+                Console.WriteLine("Unity service behavior enabled.");
+
+                selfHost.Open();
+                Console.WriteLine("AuthorisationManager service is open.");
+                Console.WriteLine("Press any key to close.");
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error starting the AuthorisationManager service : " + ex.Message);
+                selfHost.Abort();
+                Console.ReadLine();
+            }
         }
     }
 }
