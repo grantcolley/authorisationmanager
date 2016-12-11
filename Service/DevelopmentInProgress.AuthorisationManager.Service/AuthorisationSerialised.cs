3057ef0198cf3d3c866e687ac810a851d9c29176
grantcolley <grantcolley@yahoo.com>
2016-12-11 16:28:48 +0000

    Remove obsolete class AuthorisationSerialised.cs
    Remove obsolete class AuthorisationSerialised.cs

--- Service/DevelopmentInProgress.AuthorisationManager.Service/AuthorisationSerialised.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevelopmentInProgress.AuthorisationManager.Service
-{
-    public class AuthorisationSerialised
-    {
-        public string Activities { get; set; }
-        public string Roles { get; set; }
-        public string UserAuthorisations { get; set; }
-    }
-}
