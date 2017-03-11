e20143c38b0fcd0329359f4ea1b75d3f20f4e04c
grantcolley <grantcolley@yahoo.com>
2017-03-11 18:27:30 +0000

    Remove default routing from config.
    Remove default routing from config.

--- Service/WebAPI/App_Start/WebApiConfig.cs
+++ Service/WebAPI/App_Start/WebApiConfig.cs
@@ -13,12 +13,6 @@ namespace DevelopmentInProgress.AuthorisationManager.WebAPI
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
