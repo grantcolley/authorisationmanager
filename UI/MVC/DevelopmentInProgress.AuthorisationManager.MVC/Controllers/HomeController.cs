6b75ed8d737fc8b838c0462efa8292c43b56dc31
grantcolley <grantcolley@yahoo.com>
2017-05-04 20:16:25 +0100

    Work in progress...
    Work in progress...

--- UI/MVC/DevelopmentInProgress.AuthorisationManager.MVC/Controllers/HomeController.cs
+++ UI/MVC/DevelopmentInProgress.AuthorisationManager.MVC/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DevelopmentInProgress.AuthorisationManager.MVC.Controllers
 {
@@ -15,14 +11,14 @@ namespace DevelopmentInProgress.AuthorisationManager.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Authorisation Manager.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Details.";
 
             return View();
         }
