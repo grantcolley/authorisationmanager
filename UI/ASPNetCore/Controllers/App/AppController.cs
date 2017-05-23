a284ab95f4d4237cdf612ba1849cdd28e9f33965
grantcolley <grantcolley@yahoo.com>
2017-05-23 14:07:37 +0100

    Add AppController
    Add AppController

---
+++ UI/ASPNetCore/Controllers/App/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevelopmentInProgress.AuthorisationManager.ASP.Net.Core.Controllers.App
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- UI/ASPNetCore/Startup.cs
+++ UI/ASPNetCore/Startup.cs
@@ -12,6 +12,7 @@ namespace DevelopmentInProgress.AuthorisationManager.ASP.Net.Core
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,9 +25,12 @@ namespace DevelopmentInProgress.AuthorisationManager.ASP.Net.Core
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" });
             });
         }
     }
