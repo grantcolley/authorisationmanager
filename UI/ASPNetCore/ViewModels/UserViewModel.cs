08de81004adda619d84d0a5b18c9f91905b4cc5e
grantcolley <grantcolley@yahoo.com>
2017-06-24 21:42:27 +0100

    Create view model for user
    Create view model for user

---
+++ UI/ASPNetCore/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevelopmentInProgress.AuthorisationManager.ASP.Net.Core.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
