dc554579af32a6a90b5fb124bd828428f1758d79
grantcolley <grantcolley@yahoo.com>
2017-07-10 21:30:26 +0100

    Adding View Component.
    Adding View Component.

---
+++ UI/ASPNetCore/ViewComponents/UserViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using DevelopmentInProgress.AuthorisationManager.ASP.Net.Core.ViewModels;
+
+namespace DevelopmentInProgress.AuthorisationManager.ASP.Net.Core.ViewComponents
+{
+    [ViewComponent(Name="User")]
+    public class UserViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(new UserViewModel());
+        }
+    }
+}
