054dd9102ae1d71e0f6a31e7ad160ff6da5eab55
grantcolley <grantcolley@yahoo.com>
2017-07-19 20:07:24 +0100

    Remove alerts from javascript
    Remove alerts from javascript

--- UI/ASPNetCore/wwwroot/js/site.js
+++ UI/ASPNetCore/wwwroot/js/site.js
@@ -1,13 +1,12 @@
 ﻿
+
 $(document).ready(function () {
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        alert('set ' + $(this).attr('href'));
         localStorage.setItem('activeTab', $(this).attr('href'));
     });
 
     var activeTab = localStorage.getItem('activeTab');
     if (activeTab) {
         $('[href="' + activeTab + '"]').tab('show');
-        alert('get ' + activeTab);
     }
 });
