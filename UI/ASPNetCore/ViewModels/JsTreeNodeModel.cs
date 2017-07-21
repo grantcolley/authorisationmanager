ede9a0d2d3fd562db19f1c1fc3dbbb04c4b02b05
grantcolley <grantcolley@yahoo.com>
2017-07-21 20:22:57 +0100

    Initial wire in jstree.
    Initial wire in jstree.

---
+++ UI/ASPNetCore/ViewModels/JsTreeNodeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DevelopmentInProgress.AuthorisationManager.ASP.Net.Core.ViewModels
+{
+    public class JsTreeNodeModel
+    {
+        public string id { get; set; }
+        public string parent { get; set; }
+        public string text { get; set; }
+        public string icon { get; set; }
+        public string state { get; set; }
+        public bool opened { get; set; }
+        public bool disabled { get; set; }
+        public bool selected { get; set; }
+        public string li_attr { get; set; }
+        public string a_attr { get; set; }
+    }
+}
