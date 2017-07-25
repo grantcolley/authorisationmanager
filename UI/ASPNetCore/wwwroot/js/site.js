54b5b86917c97556e6b873ea357c1038a1c9d594
grantcolley <grantcolley@yahoo.com>
2017-07-25 08:48:09 +0100

    Refresh after tab selection changed.
    Refresh after tab selection changed.

--- UI/ASPNetCore/wwwroot/js/site.js
+++ UI/ASPNetCore/wwwroot/js/site.js
@@ -1,25 +1,26 @@
 ﻿
 
 $(document).ready(function () {    
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-
-        localStorage.setItem('activeTab', $(this).attr('href'));
-        alert($(this).attr('href'));
-        $('#userTree').jstree({
-            'core': {
-                'data': {
-                    'url': '/Authorisation/GetTree?' + $(this).attr('href'),
-                    'data': function (node) {
-                        return { 'id': node.id };
-                    }
+    $('#userTree').jstree({
+        'core': {
+            'data': {
+                'url': '/Authorisation/GetTree?tab=' + $(this).attr('href'),
+                'data': function (node) {
+                    return { 'id': node.id };
                 }
             }
-        });
+        }
+    });
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        localStorage.setItem('activeTab', $(this).attr('href'));
+
+        $('#userTree').jstree(true).settings.core.url = '/Authorisation/GetTree?tab=' + $(this).attr('href');
+        $('#userTree').jstree(true).refresh();
     });
 
     var activeTab = localStorage.getItem('activeTab');
     if (activeTab) {
         $('[href="' + activeTab + '"]').tab('show');
-        alert("test 2");
     }
 });
